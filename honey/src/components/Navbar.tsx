990963f298ff17977bfa6639b80880f4e91f30a2
Sebastian <sebastian.kosciolek2000@gmail.com>
2024-10-16 13:48:28 +0200

    fix bugs

--- honey/src/components/Navbar.tsx
+++ honey/src/components/Navbar.tsx
@@ -1,6 +1,5 @@
-import menu from '../assets/menu.svg'
-import close from '../assets/close.svg'
-
+import menu from '../assets/menu.svg';
+import close from '../assets/close.svg';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import '../styles/Navbar.css';
@@ -20,15 +19,23 @@ const Navbar = () => {
       }
   }
 
+  function closeSidebar() {
+      const sidebar = document.querySelector('.sidebar');
+      if (sidebar && sidebar.classList.contains('active')) {
+          sidebar.classList.remove('active');
+          (sidebar as HTMLElement).style.display = 'none';
+      }
+  }
+
   return (
     <nav>
       <ul className='sidebar'>
-      <li className='main-close-icon-div' onClick={showSidebar} ><a><img className='main-close-icon' src={close}/></a></li>
-        <li><Link to="/home"><a className='menu-sidebar-li-element'>Home</a></Link></li>
-        <li><Link to="/menu"><a className='menu-sidebar-li-element'>Menu</a></Link></li>
-        <li><Link to="/aboutus"><a className='menu-sidebar-li-element'>About Us</a></Link></li>
-        <li><Link to="/careers"><a className='menu-sidebar-li-element'>Careers</a></Link></li>
-        <li><Link to="/contact"><a className='menu-sidebar-li-element'>Contact</a></Link></li>
+        <li className='main-close-icon-div' onClick={showSidebar}><a><img className='main-close-icon' src={close}/></a></li>
+        <li><Link to="/home" onClick={closeSidebar}><a className='menu-sidebar-li-element'>Home</a></Link></li>
+        <li><Link to="/menu" onClick={closeSidebar}><a className='menu-sidebar-li-element'>Menu</a></Link></li>
+        <li><Link to="/aboutus" onClick={closeSidebar}><a className='menu-sidebar-li-element'>About Us</a></Link></li>
+        <li><Link to="/careers" onClick={closeSidebar}><a className='menu-sidebar-li-element'>Careers</a></Link></li>
+        <li><Link to="/contact" onClick={closeSidebar}><a className='menu-sidebar-li-element'>Contact</a></Link></li>
       </ul>
 
       <ul className='navbar-list-logo'>
@@ -38,10 +45,10 @@ const Navbar = () => {
         <li className='hide-on-mobile'><Link to="/aboutus"><a>About Us</a></Link></li>
         <li className='hide-on-mobile'><Link to="/careers"><a>Careers</a></Link></li>
         <li className='hide-on-mobile'><Link to="/contact"><a>Contact</a></Link></li>
-        <li className='main-menu-icon-div' onClick={showSidebar} ><a><img className='main-menu-icon' src={menu}/></a></li>
+        <li className='main-menu-icon-div' onClick={showSidebar}><a><img className='main-menu-icon' src={menu}/></a></li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- honey/src/pages/Careers.tsx
+++ honey/src/pages/Careers.tsx
@@ -68,26 +68,29 @@ const Careers: React.FC = () => {
 
       {/* Modal for Job Form */}
       <Dialog open={isOpen} onClose={closeModal} className="relative z-10">
-        <div className="fixed inset-0 bg-black bg-opacity-30" aria-hidden="true" />
-        <div className="fixed inset-0 flex items-center justify-center">
-          <Dialog.Panel className="mx-auto w-full max-w-md rounded bg-white p-6">
-            <Dialog.Title className="text-lg font-bold">Aplikacja na stanowisko</Dialog.Title>
-            {selectedJobId && (
-              <JobForm
-                jobId={selectedJobId!}
-                jobTitle={JobData.find((job) => job.id === selectedJobId)?.title || ''}
-                onClose={closeModal}
-              />
-            )}
-            <button
-              onClick={closeModal}
-              className="mt-4 text-white bg-red-600 hover:bg-red-500 px-4 py-2 rounded"
-            >
-              Zamknij
-            </button>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
+  <div className="fixed inset-0 bg-black bg-opacity-30" aria-hidden="true" />
+  <div className="fixed inset-0 flex items-center justify-center p-4 sm:p-6 lg:p-8">
+    <Dialog.Panel className="mx-auto w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl rounded bg-white p-4 sm:p-6 lg:p-8">
+      <Dialog.Title className="text-lg font-bold text-center sm:text-left">
+        Aplikacja na stanowisko
+      </Dialog.Title>
+      {selectedJobId && (
+        <JobForm
+          jobId={selectedJobId!}
+          jobTitle={JobData.find((job) => job.id === selectedJobId)?.title || ''}
+          onClose={closeModal}
+        />
+      )}
+      <button
+        onClick={closeModal}
+        className="mt-4 w-full sm:w-auto text-white bg-red-600 hover:bg-red-500 px-4 py-2 rounded"
+      >
+        Zamknij
+      </button>
+    </Dialog.Panel>
+  </div>
+</Dialog>
+
     </div>
   );
 };
